d88cc4e8e0f6484eba92ce9d0acb40c7b5a458bd
Carlos-DR <60520677+Carlos-DR@users.noreply.github.com>
2020-02-29 20:02:50 +0100

    ideas previas del index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Página de Inicio</title>
+    </head>
+    <body>
+        <!-- Si no has iniciado sesión se muestra esto -->
+        <form action="registro.php" method="post">
+        </form>
+
+        <!-- Si la sesión está iniciada con un usuario diferente a administrador se muestra esto -->
+        <form action="form.php" method="post">
+        </form>
+
+        <!-- Si el usuario es el administrador se muestra esto -->
+        
+    </body>
+</html>
