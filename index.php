9a2496471e260db2f5064057d1f268df68b4958c
Víctor González Morales <61558694+victorgomo@users.noreply.github.com>
2020-03-04 12:12:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -28,6 +28,43 @@ session_start();
             echo "<form method='post' action='img_registro.php'>
                 <input type='submit' name='uploadimg' value='Publicar foto'>
                 </form>";
+
+            if ($_SESSION['sesion'] == 'admin') {
+                echo "<table>";
+                echo "<tr>";
+                echo "<th>Usuario</th><th>Título</th><th></th><th>Pie de foto</th><th>Fecha</th><th>Editar</th><th>Borrar</th>";
+                echo "</tr>";
+                echo "<tr>";
+                require_once './connection.php';
+                $connect = new connection();
+                $sql = "SELECT u.user,i.name,i.description,i.fecha,i.ubication FROM imagenes i, usuarios u WHERE i.userid = u.id ORDER BY i.id DESC";
+                $result = $connect->execSQL($sql);
+               
+                while ($lane = $result->fetch_assoc()) {
+                echo "<tr><td>" . $lane['user'] . "</td><td>" . $lane['name'] . "</td><td><img height='100px' width='100px' src='" . $lane['ubication'] . "'></td>"
+                . "<td>" . $lane['description'] . "</td><td>" . $lane['fecha'] . "</td>"
+                . "<td><form method='post' action='img_update.php'><button type='submit' name='edit_img'>·</button></form></td>"
+                . "<td><form method='post' action='img_delete.php'><button type='submit' name='del_img'>·</button></form></td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "<table>";
+                echo "<tr>";
+                echo "<th>Usuario</th><th>Título</th><th></th><th>Pie de foto</th><th>Fecha</th>";
+                echo "</tr>";
+                echo "<tr>";
+                require_once './connection.php';
+                $connect = new connection();
+                $sql = "SELECT u.user,i.name,i.description,i.fecha,i.ubication FROM imagenes i, usuarios u WHERE i.userid = u.id ORDER BY i.id DESC";
+                $result = $connect->execSQL($sql);
+
+                while ($lane = $result->fetch_assoc()) {
+                echo "<tr><td>" . $lane['user'] . "</td><td>" . $lane['name'] . "</td><td><img height='100px' width='100px' src='" . $lane['ubication'] . "'></td>"
+                . "<td>" . $lane['description'] . "</td><td>" . $lane['fecha'] . "</td></tr>";
+                }
+                echo "</table>";
+                
+                }
         } else {
 
             echo "<form method='post' action='login.php'>
@@ -37,44 +74,22 @@ session_start();
             echo "<form method='post' action='registro.php'>
                 <input type='submit' name='update' value='Registrarse'>
                 </form>";
-        }
 
-        if (!$_SESSION['sesion'] == 'admin') {
-            
-        
-        echo "<table>";
-        echo "<tr>";
-        echo "<th>Usuario</th><th>Título</th><th></th><th>Pie de foto</th><th>Fecha</th>";
-        echo "</tr>";
-        echo "<tr>";
-        require_once './connection.php';
-        $connect = new connection();
-        $sql = "SELECT u.user,i.name,i.description,i.fecha,i.ubication FROM imagenes i, usuarios u WHERE i.userid = u.id ORDER BY i.id DESC";
-        $result = $connect->execSQL($sql);
+            echo "<table>";
+            echo "<tr>";
+            echo "<th>Usuario</th><th>Título</th><th></th><th>Pie de foto</th><th>Fecha</th>";
+            echo "</tr>";
+            echo "<tr>";
+            require_once './connection.php';
+            $connect = new connection();
+            $sql = "SELECT u.user,i.name,i.description,i.fecha,i.ubication FROM imagenes i, usuarios u WHERE i.userid = u.id ORDER BY i.id DESC";
+            $result = $connect->execSQL($sql);
 
-        while ($lane = $result->fetch_assoc()) {
+            while ($lane = $result->fetch_assoc()) {
             echo "<tr><td>" . $lane['user'] . "</td><td>" . $lane['name'] . "</td><td><img height='100px' width='100px' src='" . $lane['ubication'] . "'></td>"
             . "<td>" . $lane['description'] . "</td><td>" . $lane['fecha'] . "</td></tr>";
-        echo "</table>";
-        }
-        } else {
-        echo "<table>";
-        echo "<tr>";
-        echo "<th>Usuario</th><th>Título</th><th></th><th>Pie de foto</th><th>Fecha</th><th>Editar</th><th>Borrar</th>";
-        echo "</tr>";
-        echo "<tr>";
-        require_once './connection.php';
-        $connect = new connection();
-        $sql = "SELECT u.user,i.name,i.description,i.fecha,i.ubication FROM imagenes i, usuarios u WHERE i.userid = u.id ORDER BY i.id DESC";
-        $result = $connect->execSQL($sql);
-
-        while ($lane = $result->fetch_assoc()) {
-            echo "<tr><td>" . $lane['user'] . "</td><td>" . $lane['name'] . "</td><td><img height='100px' width='100px' src='" . $lane['ubication'] . "'></td>"
-            . "<td>" . $lane['description'] . "</td><td>" . $lane['fecha'] . "</td></tr>"
-            . "<td><form method='post' action='img_update.php'><button type='submit' name='edit' value='#'></button></form></td>"
-            . "<td><form method='post' action='img_delete.php'><button type='submit' name='delete' value='#'></button></form></td>";
-        echo "</table>";
-        }
+            }
+            echo "</table>";
         }
         ?>
 
