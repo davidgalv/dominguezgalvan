e704befa960e2207ee42a5cc2c4baf959bf07be5
Galvan <32952162+davidgalv@users.noreply.github.com>
2020-03-04 20:30:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -255,28 +255,6 @@ session_start();
             echo "</table>";
             echo "</div>";
             
-            if (isset($_POST['del_img'])) {
-            echo    "<div class='modal fade' id='sino' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-                <div class='modal-dialog' role='document'>
-                  <div class='modal-content>
-                    <div class='modal-header'>
-                      <h5 class='modal-title' id='sino'>Modal title</h5>
-                      <button type='button class='close' data-dismiss='modal' aria-label='Close'>
-                        <span aria-hidden='true'>&times;</span>
-                      </button>
-                    </div>
-                    <div class='modal-body'>
-                      ...
-                    </div>
-                    <div class='modal-footer'>
-                      <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
-                      <button type='button' class='btn btn-danger'>Save changes</button>
-                    </div>
-                  </div>
-                </div>
-              </div>";
-            }
-            
             
             }
         ?>
